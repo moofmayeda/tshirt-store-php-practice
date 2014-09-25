8db54a1d15a9b909a9732d686ecf74f452591804
= <=>
2014-09-25 09:43:29 -0700

    display shirts for sale

--- shirts.php
+++ shirts.php
@@ -1,10 +1,24 @@
 <?php
+
+$products = array();
+$products[101] = "Logo Shirt, Red";
+$products[102] = "Mike the Frog Shirt, Black";
+$products[103] = "Mike the Fros Shirt, Blue";
+$products[104] = "Logo Shirt, Green";
+
+?><?php
 $pageTitle = "Shirts";
 $section = "shirts";
 include('inc/header.php');
 ?>
   <div class="section page">
-    <h1>Mike&rsquo;s Full Catalog of Shirts</h1>
-
+    <div class="wrapper">
+      <h1>Mike's Full Catalog of Shirts</h1>
+      <ul>
+          <?php foreach($products as $product) { ?>
+          <li><?php echo $product ?></li>
+          <?php } ?>
+      </ul>
+    </div>
   </div>
 <?php include('inc/footer.php'); ?>
